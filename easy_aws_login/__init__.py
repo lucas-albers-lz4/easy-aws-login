2b7c454169587e48d352cd151130bcc01fb532c6
Lucas Albers <lucas.b.albers@gmail.com>
2025-11-22 10:44:49 -0700

    auto generated version bump, and add in docstring so ruff check stops complaining

---
+++ easy_aws_login/__init__.py
@@ -0,0 +1,1 @@
+"""Easy AWS Login - CLI tool for AWS console login via federation tokens."""
--- easy_aws_login/__version__.py
+++ easy_aws_login/__version__.py
@@ -1,1 +1,3 @@
-__version__ = "0.4.1"
+"""Version information for easy-aws-login package."""
+
+__version__ = "0.4.2"
